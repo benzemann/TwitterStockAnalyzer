4b20224e8078a7b73a82f59d8d5f846777953ca0
Simon Thordal <simonthordal@gmail.com>
2015-12-09 16:31:29 +0100

    Final commit

--- site/stock_server.py
+++ site/stock_server.py
@@ -32,21 +32,31 @@ class StockAnalyzer(object):
     @cherrypy.expose
     @cherrypy.tools.json_out()
     def stock(self, symbol):
+        try:
 
-        response = stock_data_helpers.get_dates_price_volume_tweetcount_lists(self.api, symbol)
+            response = stock_data_helpers.get_dates_price_volume_tweetcount_lists(self.api, symbol)
 
-        users = response[6]
-        active_communities = set([com for com, members in self.inverse_communities.iteritems() for user in members if user in users])
-        response[0].insert(0,'x')
-        response[1].insert(0,'price')
-        response[2].insert(0,'volume')
-        response[3].insert(0,'tweet volume')
-        response[4].insert(0, 'bullish')
-        response[5].insert(0, 'bearish')
-        response[6] = list(active_communities)
+            users = response[6]
+            active_communities = set([com for com, members in self.inverse_communities.iteritems() for user in members if user in users])
+            response[0].insert(0,'x')
+            response[1].insert(0,'price')
+            response[2].insert(0,'volume')
+            response[3].insert(0,'tweet_vol')
+            response[4].insert(0, 'bullish')
+            response[5].insert(0, 'bearish')
+            response[6] = list(active_communities)
 
-        return {'data': response}
+            return {'data': response}
+        except:
+            return {'status': 500}
 
 if __name__ == '__main__':
     static_dir = os.path.dirname(os.path.abspath(__file__))
-    cherrypy.quickstart(StockAnalyzer(), '/', 'app.conf')
+    conf = {
+        '/': {'tools.staticdir.root': static_dir},
+        '/static': {
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': "static"
+        }
+    }
+    cherrypy.quickstart(StockAnalyzer(), '/', conf)
